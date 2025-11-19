c4cea4c24f5555a5c321f7a1bb1c4f9568d3ee86
Luciano Bello <bel@zurich.ibm.com>
2025-11-19 16:41:44 +0100

    remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="dynacir",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=["qiskit>=0.22,<=2"],
-    python_requires=">=3.6",
-)
